e9eb82368409e30f926899174634243078e21f0b
Chenxi Li <lichenxi.webank@gmail.com>
2019-11-19 20:22:08 +0800

    make logo centralized

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/user"
+	"strings"
 
 	"github.com/vita-dounai/Firework/repl"
 )
@@ -14,13 +15,19 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf(`
-	    ___ _                             _    
-	    / __(_)_ __ _____      _____  _ __| | __
-	   / _\ | | '__/ _ \ \ /\ / / _ \| '__| |/ /
-	  / /   | | | |  __/\ V  V / (_) | |  |   < 
-	  \/    |_|_|  \___| \_/\_/ \___/|_|  |_|\_\				
-  `)
-	fmt.Printf("\nHello %s, This is the REPL for Firework programming language.\n", user.Name)
+	greeting := fmt.Sprintf("Hello %s, This is the REPL for Firework programming language.", user.Name)
+	greetingWidth := len(greeting)
+
+	logoWidth := 42
+	startPosition := (greetingWidth - logoWidth) / 2
+	whitePrefix := strings.Repeat(" ", startPosition)
+
+	fmt.Println(whitePrefix + "  ___ _                             _     ")
+	fmt.Println(whitePrefix + "  / __(_)_ __ _____      _____  _ __| | __")
+	fmt.Println(whitePrefix + " / _\\ | | '__/ _ \\ \\ /\\ / / _ \\| '__| |/ /")
+	fmt.Println(whitePrefix + "/ /   | | | |  __/\\ V  V / (_) | |  |   < ")
+	fmt.Println(whitePrefix + "\\/    |_|_|  \\___| \\_/\\_/ \\___/|_|  |_|\\_\\")
+
+	fmt.Println(greeting)
 	repl.Start(os.Stdin, os.Stdout)
 }
